3e03d8a90f9eb2441531b263a69baffb4b8d3c92
ReidAtcheson <reid.atcheson@gmail.com>
2015-01-19 13:20:44 -0600

    Fixed test script

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,41 +2,37 @@ module TESTOCCA
 using OCCA
 using Base.Test
 
-
-function test_vectoradd(kernelfile)
+function test_vectoradd(kernelfile::String)
     entries = 5
-    device = OCCA.Device("OpenMP", 0, 0);
+
+    OpenMP_Info   = "mode = OpenMP  , schedule = compact, chunk = 10";
+    OpenCL_Info   = "mode = OpenCL  , platformID = 0, deviceID = 0";
+    CUDA_Info     = "mode = CUDA    , deviceID = 0";
+    Pthreads_Info = "mode = Pthreads, threadCount = 4, schedule = compact, pinnedCores = [0, 0, 1, 1]";
+    COI_Info      = "mode = COI     , deviceID = 0";
+
+    device = occa.device(OpenMP_Info);
 
     a  = Float32[1 - i for i in 1:entries]
     b  = Float32[i     for i in 1:entries]
     ab = Float32[0     for i in 1:entries]
 
-    o_a  = OCCA.malloc(device, a);
-    o_b  = OCCA.malloc(device, b);
-    o_ab = OCCA.malloc(device, ab);
+    o_a  = occa.malloc(device, a);
+    o_b  = occa.malloc(device, b);
+    o_ab = occa.malloc(device, ab);
 
-    addVectors = OCCA.buildKernelFromSource(device,
-                                            kernelfile,
+    addVectors = occa.buildKernelFromSource(device,
+                                            kernelFile,
                                             "addVectors")
 
-    dims = 1;
-    itemsPerGroup = 2;
-    groups = (entries + itemsPerGroup - 1)/itemsPerGroup;
-
-    OCCA.setWorkingDims(addVectors,
-                        dims, itemsPerGroup, groups);
-
-    OCCA.runKernel(addVectors,
+    occa.runKernel(addVectors,
                    (entries, Int32),
                    o_a, o_b, o_ab)
 
-    OCCA.memcpy(ab, o_ab)
+    occa.memcpy(ab, o_ab)
 
     println(ab)
-
-    return true;
 end
-
 
 
 @test test_vectoradd("addVectors.ofl") == true;
