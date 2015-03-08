441312ce5697b92e36b65d022458f5552f6039e0
ReidAtcheson <reid.atcheson@gmail.com>
2015-03-08 15:13:42 -0500

    fixing test script

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,25 +8,25 @@ include("testtypes.jl");
 
 #Test vector add kernel.
 include("test_okl_ofl_vectoradd.jl");
-@test test_okl_ofl_vectoradd(TestBackend(testserial)) == true;
-@test test_okl_ofl_vectoradd(TestBackend(testopenmp)) == true;
-@test test_okl_ofl_vectoradd(TestBackend(testopencl)) == true;
-@test test_okl_ofl_vectoradd(TestBackend(testpthreads)) == true;
-@test test_okl_ofl_vectoradd(TestBackend(testcuda)) == true;
+@test test_okl_ofl_vectoradd(testserial) == true;
+@test test_okl_ofl_vectoradd(testopenmp) == true;
+@test test_okl_ofl_vectoradd(testopencl) == true;
+@test test_okl_ofl_vectoradd(testpthreads) == true;
+@test test_okl_ofl_vectoradd(testcuda) == true;
 
-@test test_okl_ofl_vectoradd(TestBackend(testserial)) == true;
-@test test_okl_ofl_vectoradd(TestBackend(testopencl)) == true;
-@test test_okl_ofl_vectoradd(TestBackend(testpthreads)) == true;
-@test test_okl_ofl_vectoradd(TestBackend(testcuda)) == true;
+@test test_okl_ofl_vectoradd(testserial) == true;
+@test test_okl_ofl_vectoradd(testopencl) == true;
+@test test_okl_ofl_vectoradd(testpthreads) == true;
+@test test_okl_ofl_vectoradd(testcuda) == true;
 
 
 #Test reduction kernel.
 include("test_okl_reduction.jl");
-@test test_okl_reduction(TestBackend(testserial)) == true;
-@test test_okl_reduction(TestBackend(testopenmp)) == true;
-@test test_okl_reduction(TestBackend(testopencl)) == true;
-@test test_okl_reduction(TestBackend(testpthreads)) == true;
-@test test_okl_reduction(TestBackend(testcuda)) == true;
+@test test_okl_reduction(testserial) == true;
+@test test_okl_reduction(testopenmp) == true;
+@test test_okl_reduction(testopencl) == true;
+@test test_okl_reduction(testpthreads) == true;
+@test test_okl_reduction(testcuda) == true;
 
 
 
